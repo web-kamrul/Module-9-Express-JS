ef3300bdde4f7ec7b25054d0b93e348c40e8fe75
web-kamrul <webkamrul0001@gmail.com>
2024-01-28 20:34:51 +0600

    Response Status Code

--- 3-ResponseApp/index.js
+++ 3-ResponseApp/index.js
@@ -1,16 +1,25 @@
 let express = require('express'); // import express module
 app = express(); // create application
 
+// post or get
+// send or end
 // res.send --------- work in response body
 // res.end --------- work in response end
 app.get ('/one', function(req,res){
     res.send ('This is One')
 })
-app.get ('/two', function(req,res){
+app.post ('/two', function(req,res){
     res.end ('This is Two')
 })
 
-app.listen (3000, function(){
+// Response Status Code
+app.get ('/three', function (req, res){
+    res.status(404);
+    res.end ("Not Found Kamrul Islam")
+})
+
+
+app.listen (3100, function(){
     console.log ('Server Run Success')
 });
 
